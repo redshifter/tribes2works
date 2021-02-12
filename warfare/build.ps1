5c6e2c9af8e05c928dc0c8bc0391a2a02f3922dd
Red Shifter <31264630+redshifter@users.noreply.github.com>
2021-02-12 00:02:31 -0500

    renamed Warfare.vl2 to WarfareGameType.vl2

--- warfare/build.ps1
+++ warfare/build.ps1
@@ -1,3 +1,4 @@
-del Warfare.vl2
-Compress-Archive -Path missions,terrains,scripts -DestinationPath Warfare.zip
-ren Warfare.zip Warfare.vl2
+# if we're going to have a github, we might as well have a build command
+del WarfareGameType.vl2
+Compress-Archive -Path missions,terrains,scripts -DestinationPath WarfareGameType.zip
+ren WarfareGameType.zip WarfareGameType.vl2
