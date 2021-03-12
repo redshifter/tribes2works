be46b36dfcd2266ba7661251d91b76957a0414da
Red Shifter <31264630+redshifter@users.noreply.github.com>
2021-03-12 14:52:54 -0500

    only load datablocks once

--- skifree/scripts/SkiFreeDatablock.cs
+++ skifree/scripts/SkiFreeDatablock.cs
@@ -1,47 +1,82 @@
 // SkiFreeDatablock.cs
 // contains datablocks for SkiFree gametype
 
-// special gate - gives an ugly white complexion - they look weird when you aren't playing so i assume nobody will actually use them in maps...
-datablock ForceFieldBareData(SkiFreeGateField) : defaultAllSlowFieldBare {
-	fadeMS           = 1000;
-	baseTranslucency = 1.0; // if you were close enough to watch, it would look pretty cool, but you're not
-	powerOffTranslucency = 0.2;
-	powerOffColor    = "1.0 1.0 1.0";
+// datablocks should only load once
+if( !isObject(SkiFreeGateField) ) {
 
-	scrollSpeed = 1;
-};
+	// special gate - gives an ugly white complexion - they look weird when you aren't playing so i assume nobody will actually use them in maps...
+	datablock ForceFieldBareData(SkiFreeGateField) : defaultAllSlowFieldBare {
+		fadeMS           = 1000;
+		baseTranslucency = 1.0; // if you were close enough to watch, it would look pretty cool, but you're not
+		powerOffTranslucency = 0.2;
+		powerOffColor    = "1.0 1.0 1.0";
 
-// decorative gates
-datablock ForceFieldBareData(SkiFreeGateField1) : SkiFreeGateField {
-	powerOffColor = "1 0 0";
-};
-datablock ForceFieldBareData(SkiFreeGateField2) : SkiFreeGateField {
-	powerOffColor = "1 0.5 0";
-};
-datablock ForceFieldBareData(SkiFreeGateField3) : SkiFreeGateField {
-	powerOffColor = "1 1 0";
-};
-datablock ForceFieldBareData(SkiFreeGateField4) : SkiFreeGateField {
-	powerOffColor = "0 1 0";
-};
-datablock ForceFieldBareData(SkiFreeGateField5) : SkiFreeGateField {
-	powerOffColor = "0 0 1";
-};
-datablock ForceFieldBareData(SkiFreeGateField6) : SkiFreeGateField {
-	powerOffColor = "0.5 0 1";
-};
-datablock ForceFieldBareData(SkiFreeGateField7) : SkiFreeGateField {
-	powerOffColor = "1 0 1";
-};
+		scrollSpeed = 1;
+	};
 
-// triggers needed for game
-datablock TriggerData(SkiFreeTriggerSpawn) {
-	tickPeriodMS = 50;
-};
+	// decorative gates
+	datablock ForceFieldBareData(SkiFreeGateField1) : SkiFreeGateField {
+		powerOffColor = "1 0 0";
+	};
+	datablock ForceFieldBareData(SkiFreeGateField2) : SkiFreeGateField {
+		powerOffColor = "1 0.5 0";
+	};
+	datablock ForceFieldBareData(SkiFreeGateField3) : SkiFreeGateField {
+		powerOffColor = "1 1 0";
+	};
+	datablock ForceFieldBareData(SkiFreeGateField4) : SkiFreeGateField {
+		powerOffColor = "0 1 0";
+	};
+	datablock ForceFieldBareData(SkiFreeGateField5) : SkiFreeGateField {
+		powerOffColor = "0 0 1";
+	};
+	datablock ForceFieldBareData(SkiFreeGateField6) : SkiFreeGateField {
+		powerOffColor = "0.5 0 1";
+	};
+	datablock ForceFieldBareData(SkiFreeGateField7) : SkiFreeGateField {
+		powerOffColor = "1 0 1";
+	};
 
-datablock TriggerData(SkiFreeTriggerGate) {
-	tickPeriodMS = 50;
-};
+	// triggers needed for game
+	datablock TriggerData(SkiFreeTriggerSpawn) {
+		tickPeriodMS = 50;
+	};
+
+	datablock TriggerData(SkiFreeTriggerGate) {
+		tickPeriodMS = 50;
+	};
+	
+	// you can't just summon an explosion; you need to blow something up. an overridden satchel charge will be blown up when i need an explosion
+	datablock ItemData(SatchelChargeDeadstop) : SatchelChargeThrown
+	{
+	   explosion = VehicleBombExplosion;
+	   underwaterExplosion = VehicleBombExplosion;
+	   maxDamage = 0.1;
+	   kickBackStrength = 0;
+	   armDelay = 1;
+	   computeCRC = false;
+	};
+
+	// SkiFreeSpawnPlatform just explodes into a platform (though i didn't use the actual object wired to explode, that would just be tacky)
+	datablock StaticShapeData(SkiFreeSpawnPlatform) {
+		catagory = "SkiFree Objects";
+		className = "Spawn Platform";
+		isInvincible = true;
+		needsNoPower = true;
+		alwaysAmbient = true;
+		shapeFile = "beacon.dts";
+	};
+
+	datablock StaticShapeData(SkiFreeCustomGate) {
+		catagory = "SkiFree Objects";
+		className = "Gate Marker";
+		isInvincible = true;
+		needsNoPower = true;
+		alwaysAmbient = true;
+		shapeFile = "beacon.dts";
+	};
+
+}
 
 function SkiFreeTriggerSpawn::onEnterTrigger(%this, %trigger, %player) {
 	if( !$missionRunning ) return; // this check is needed so it doesn't call this at end of mission
@@ -73,30 +108,9 @@ function SkiFreeTriggerGate::onLeaveTrigger(%this, %trigger, %player) {
 	// o shit wut up
 }
 
-// you can't just summon an explosion; you need to blow something up. an overridden satchel charge will be blown up when i need an explosion
-datablock ItemData(SatchelChargeDeadstop) : SatchelChargeThrown
-{
-   explosion = VehicleBombExplosion;
-   underwaterExplosion = VehicleBombExplosion;
-   maxDamage = 0.1;
-   kickBackStrength = 0;
-   armDelay = 1;
-   computeCRC = false;
-};
-
 function SatchelChargeDeadstop::onCollision(%data,%obj,%col) {
 	// please don't try to pick up the exploding bomb, even if it's only decorative
 }
-
-// SkiFreeSpawnPlatform just explodes into a platform (though i didn't use the actual object wired to explode, that would just be tacky)
-datablock StaticShapeData(SkiFreeSpawnPlatform) {
-	catagory = "SkiFree Objects";
-	className = "Spawn Platform";
-	isInvincible = true;
-	needsNoPower = true;
-	alwaysAmbient = true;
-	shapeFile = "beacon.dts";
-};
 
 function SkiFreeSpawnPlatform::onAdd(%this, %obj) {
 	// come back when we know where this object is
@@ -130,15 +144,6 @@ function SkiFreeSpawnPlatform::doAdd(%this, %obj) {
 	
 	%obj.delete();
 }
-
-datablock StaticShapeData(SkiFreeCustomGate) {
-	catagory = "SkiFree Objects";
-	className = "Gate Marker";
-	isInvincible = true;
-	needsNoPower = true;
-	alwaysAmbient = true;
-	shapeFile = "beacon.dts";
-};
 
 function SkiFreeCustomGate::onAdd(%this, %obj) {
 	Parent::onAdd(%this, %obj);
