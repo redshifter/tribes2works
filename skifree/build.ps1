b96b98dbc64534af14a89e4acb3e4ceb8cd6681a
Red Shifter <31264630+redshifter@users.noreply.github.com>
2021-04-12 22:22:29 -0400

    changed build command

--- skifree/build.ps1
+++ skifree/build.ps1
@@ -1,4 +1,4 @@
 # if we're going to have a github, we might as well have a build command
 del SkiFreeGameType.vl2
-Compress-Archive -Path scripts,missions,terrains,audio,textures,other,../LICENSE -DestinationPath SkiFreeGameType.zip
+Compress-Archive -Path scripts,missions,terrains,gui,other,../LICENSE -DestinationPath SkiFreeGameType.zip
 move SkiFreeGameType.zip SkiFreeGameType.vl2
